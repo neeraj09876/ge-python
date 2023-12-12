69cc478c8619ee69fd1248d39ed161ea050ecb45
neeraj09876 <145014880+neeraj09876@users.noreply.github.com>
2023-12-12 09:28:24 +0530

    Create Practical10.py

---
+++ Practical10.py
@@ -0,0 +1,6 @@
+def create_cubes_dictionary():
+    cubes_dict = {num: num**3 for num in range(1, 6)}
+    print(cubes_dict)
+
+# Call the function to print the dictionary
+create_cubes_dictionary()
